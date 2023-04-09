af6e56cd1486d700cd314c9c142cb562435823c4
AdMrm <114076155+AdMrm@users.noreply.github.com>
2023-04-09 20:24:12 +0000

    Update postPage.js

--- client/src/pages/postPage.js
+++ client/src/pages/postPage.js
@@ -9,7 +9,7 @@ export default function PostPage() {
   const {userInfo} = useContext(UserContext);
   const {id} = useParams();
   useEffect(() => {
-    fetch(`https://urskin.onrender.com/post/${id}`)
+    fetch(`http://localhost:4000/post/${id}`)
       .then(response => {
         response.json().then(postInfo => {
           setPostInfo(postInfo);
