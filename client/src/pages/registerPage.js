dbb45572b5e1e2bfb5a70c1f16678ce6cdb21c59
AdMrm <114076155+AdMrm@users.noreply.github.com>
2023-04-26 21:32:49 +0000

    Update registerPage.js

--- client/src/pages/registerPage.js
+++ client/src/pages/registerPage.js
@@ -6,7 +6,7 @@ export default function RegisterPage() {
   async function register(ev) {
       ev.preventDefault();
       
-      const response = await fetch('http://localhost:4000/register', {
+      const response = await fetch('https://urskin.onrender.com/register', {
           method: 'POST',
           body: JSON.stringify({ username, password }),
           headers: { 'content-type': 'application/json' },
