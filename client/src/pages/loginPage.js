3a2bf978fa3619a1ad69509d988e06f2acc8d8ba
AdMrm <114076155+AdMrm@users.noreply.github.com>
2023-04-26 21:31:48 +0000

    Update loginPage.js

--- client/src/pages/loginPage.js
+++ client/src/pages/loginPage.js
@@ -10,7 +10,7 @@ import {UserContext} from '../userContext'
         const { setUserInfo } = useContext(UserContext);
         async function login(ev) {
             ev.preventDefault();
-            const response = await fetch('http://localhost:4000/login', {
+            const response = await fetch('https://urskin.onrender.com/login', {
                 method: 'POST',
                 body: JSON.stringify({ username, password }),
                 headers: { 'Content-Type': 'application/json' },
