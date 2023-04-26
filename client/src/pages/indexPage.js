651ad71f53236f1b1809671e1a305be0223ab208
AdMrm <114076155+AdMrm@users.noreply.github.com>
2023-04-26 21:31:15 +0000

    Update indexPage.js

--- client/src/pages/indexPage.js
+++ client/src/pages/indexPage.js
@@ -5,7 +5,7 @@ import Off from "../components/off";
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:4000/post').then(response => {
+    fetch('https://urskin.onrender.com/post').then(response => {
       response.json().then(posts => {
         setPosts(posts);
       });
